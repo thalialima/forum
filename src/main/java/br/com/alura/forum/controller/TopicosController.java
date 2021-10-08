1d06d67020fc6199d8d2a6fb4e8d0f5f09421bda
thalialima <thaliairene20@gmail.com>
2021-10-08 16:23:27 -0300

    Adicionando método POST

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,27 +1,38 @@
 package br.com.alura.forum.controller;
 
-import java.util.Arrays;
+import java.net.URI;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.alura.forum.controller.dto.TopicoDTO;
-import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 
 //primeiro endpoint
 //usando @RestController, o Spring assume que por padrão todos os métos já vão conter
 //o @ResponseBody
 @RestController
+@RequestMapping("/topicos")
 public class TopicosController {
 
 	@Autowired
 	private TopicoRepository topicoRepository;
+	
+	@Autowired
+	private CursoRepository cursoRepository;
 
-	@RequestMapping("/topicos")
+	@GetMapping
 	public List<TopicoDTO> lista(String nomeCurso) {
 		if (nomeCurso == null) {
 			// findAll faz uma consulta carregando todos os registros do BD
@@ -32,4 +43,25 @@ public class TopicosController {
 			return TopicoDTO.converter(topicos);
 		}
 	}
+	
+	@PostMapping
+	//TopocoForm é um DTO para receber dados do cliente
+	//@RequestBody indica que os parâmentros devem ser pegos do corpo da requisição
+	//o código 201 indica que algo foi criado com sucesso no servidor
+	public ResponseEntity<TopicoDTO> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder) {
+		Topico topico = form.toTopico(cursoRepository);
+		topicoRepository.save(topico);
+		
+		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+		//devolve o código 201
+		return ResponseEntity.created(uri).body(new TopicoDTO(topico));
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- src/main/java/br/com/alura/forum/modelo/Topico.java
+++ src/main/java/br/com/alura/forum/modelo/Topico.java
@@ -38,6 +38,12 @@ public class Topico {
 		// TODO Auto-generated constructor stub
 	}
 
+	public Topico(String titulo, String mensagem, Curso curso) {
+		this.titulo = titulo;
+		this.mensagem = mensagem;
+		this.curso = curso;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
